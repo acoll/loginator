17a61291d0aa16362c389e13f011cd8a4085766b
Adam <adamcoll.ac@gmail.com>
2015-11-23 15:24:28 -0500

    clean up test

--- test.js
+++ test.js
@@ -1,36 +1,44 @@
-var logger = require('./')('test123');
+var logger = require('./')(__filename.replace(__dirname, ''));
 
+// turn off colors at the root
 logger.useColors = false;
 logger.showWarn = false;
 
-logger.info('HELLO1');
-logger.warn('HELLO');
-logger.debug('hello');
+logger.info('THIS IS THE ROOT LOGGER.', 'MULTIPLE THINGS CAN ALWAYS BE PASSED');
+var x = {
+	name: 'Adam',
+	age: 1000
+};
 
-var sub = logger('more more');
+logger.info('Log an object if you want to:', x);
 
-var subDown = logger(() => (new Date));
+// Make a child logger for logging with the current user's id
+var userLogger = logger('user=15732');
+userLogger.debug('Login event');
 
-subDown.info('bleh');
-setTimeout(() => subDown.debug('LSKDJFLSKDJFLSDKFJ'), 1500);
+// Make a child logger with a functin for the context value.
+var intervalLogger = userLogger(() => new Date);
+setInterval(() => intervalLogger.info('Hello Interval'), 2000);
 
-sub.useColors = true;
-sub.showWarn = true;
-sub.info('stuff in sub');
-sub.warn('sldkjsldkf');
+var logger1 = logger('logger-1');
 
-var sub2 = sub('another layer');
+logger1.useColors = true;
+logger1.showWarn = true;
+logger1.info('HELLO INFO');
+logger1.debug('HELLO DEBUG');
+logger1.warn('HELLO WARN');
+logger1.error('HELLO ERROR');
 
-sub2.info('Deep');
+var logger2 = logger1('logger-2');
+logger2.info('HELLO INFO');
+logger2.debug('HELLO DEBUG');
+logger2.warn('HELLO WARN');
+logger2.error('HELLO ERROR');
 
-sub2.error('bleh');
+var logger8 = logger2('logger-3')('logger-4')('logger-5')('logger-6')('logger-7')('logger-8');
+logger8.info('HELLO DEEP LOGGER');
 
-sub2.error('bleh');
-sub2.showError = false;
-sub2.error('bleh');
-
-sub2.error('NOPE WE WONT SEE THIS');
-
-var soDeep = logger('stuff')('thing')('more')('later')('way down');
-
-soDeep.error('SWEET');
+setTimeout(() => {
+	intervalLogger.debug('Yea lets do colors again.');
+	intervalLogger.useColors = true;
+}, 4000);
